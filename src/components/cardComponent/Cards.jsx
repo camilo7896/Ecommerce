e25e59c466b0fbb3872bc696104cb8cf0f6ea14e
Cristian Camilo Garcia <cgarcia@caballo.com.co>
2023-02-07 15:25:26 -0500

    Se crearon estados para el carrito de compras

--- src/components/cardComponent/Cards.jsx
+++ src/components/cardComponent/Cards.jsx
@@ -3,25 +3,39 @@ import ProductData from '../../data/products.json'
 import Styles from './cards.module.css'
 
 export default function Cards() {
-  const [product, setProduct] = useState(ProductData)
+
+  {/**************  Estados  ***************/}
+{/*Estado de los productos */}
+  const [dataProduct, setDataProduct] = useState(ProductData)
+  {/**Estado del carrito**/}
+  const [allProduct, setAllproduct] = useState([])
+  {/*Estado del contador */}
   const [count, setCount] = useState(0)
+  {/*Total de productos */}
+const [total, setTotal]=useState(0)
+
+const onAddProduc =product=>{
+  setAllproduct([...allProduct, product])
+}
+  console.log(allProduct, count)
   return (
     <>
       <div className={Styles.container}>
-        {product.products.map((item) => {
+        {/* Recorrido de la data */}
+        {dataProduct.products.map((product) => {
           return (
-            <div key={item.id} className={Styles.containerCard}>
+            <div key={product.id} className={Styles.containerCard}>
               <div className="card w-72 bg-base-100 shadow-xl m-6">
                 <figure className="px-10 pt-10">
-                  <img src={item.image} alt="Shoes" className="rounded-xl" />
+                  <img src={product.image} alt="Shoes" className="rounded-xl" />
                 </figure>
-                <div className="card-body items-center text-center">
-                  <div className="justify-items-end">
-                    <span>$ {item.price}</span>
+                <div className="card-body products-center text-center">
+                  <div className="justify-products-end">
+                    <span>$ {product.price}</span>
                   </div>
-                  <h2 className="card-title">{item.name}</h2>
-                  <p>{item.description}</p>
-                  <div className="flex m-2 items-center">
+                  <h2 className="card-title">{product.name}</h2>
+                  <p>{product.description}</p>
+                  <div className="flex m-2 products-center">
                     <button
                       onClick={() => setCount(count - 1)}
                       className="m-5  btn btn-outline btn-secondary"
@@ -39,7 +53,7 @@ export default function Cards() {
                     </button>
                   </div>
                   <div className="card-actions">
-                    <button className="btn btn-success">
+                    <button className="btn btn-success" onClick={()=> onAddProduc(product)}>
                       agregar al carrito
                     </button>
                   </div>
