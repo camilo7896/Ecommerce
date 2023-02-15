d9a605e8de06008aa6ac35745e33ba3a1d274d6a
Cristian Camilo Garcia <cgarcia@caballo.com.co>
2023-02-15 16:24:47 -0500

    Se colocaron condicionales en el componente Carrito

--- src/components/carrito/Form.jsx
+++ src/components/carrito/Form.jsx
@@ -1,13 +1,18 @@
 import { text } from '@fortawesome/fontawesome-svg-core'
 import React from 'react'
+import { useGlobalContext } from '../../provider/DataProvider'
 
 export default function Form() {
+  const {allProduct,setAllproduct, total, message} = useGlobalContext()
   return (
     <div>
         <form>
-            <label>Nombre</label>
-            <input type={text} required></input>
-            <label type={Number} required></label>
+     
+            <input type="text" placeholder="Nombres" className="input input-bordered input-info w-full max-w-xs" />
+           
+            <input type="Number" placeholder="Celular" className="input input-bordered input-info w-full max-w-xs" />
+
+            <button className="btn btn-block bg-success" onClick={sendList}>Enviar Pedido</button>
         </form>
     </div>
   )
